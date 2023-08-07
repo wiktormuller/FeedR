52ec3aa975fd342fec78860b2fbc19b58b7a8dc9
Wiktor Muller <biznes.wiktor@gmail.com>
2023-08-07 21:24:57 +0200

    Add Interlocked Synchronization when trying to start generating pricing in mutli-thread environment

--- src/Feeds/Quotes/FeedR.Feeds.Quotes/Pricing/Services/PricingBackgroundService.cs
+++ src/Feeds/Quotes/FeedR.Feeds.Quotes/Pricing/Services/PricingBackgroundService.cs
@@ -4,26 +4,57 @@ namespace FeedR.Feeds.Quotes.Pricing.Services
 {
     internal class PricingBackgroundService : BackgroundService
     {
+        private int _runningStatus; // 0 - disabled or 1 - already running
         private readonly IPricingGenerator _generator;
         private readonly PricingRequestsChannel _channel;
+        private readonly ILogger<PricingBackgroundService> _logger;
 
-        public PricingBackgroundService(IPricingGenerator generator, PricingRequestsChannel channel)
+        public PricingBackgroundService(IPricingGenerator generator,
+            PricingRequestsChannel channel, 
+            ILogger<PricingBackgroundService> logger)
         {
             _generator = generator;
             _channel = channel;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await foreach (var request in _channel.Requests.Reader.ReadAllAsync())
+            _logger.LogInformation("Pricing background service has started");
+            await foreach (var request in _channel.Requests.Reader.ReadAllAsync(stoppingToken))
             {
+                _logger.LogInformation($"Pricing background service has received the request: {request.GetType().Name}.");
+
                 var _ = request switch
                 {
-                    StartPricing => _generator.StartAsync(),
-                    StopPricing => _generator.StopAsync(),
+                    StartPricing => StartGeneratorAsync(),
+                    StopPricing => StopGeneratorAsync(),
                     _ => Task.CompletedTask
                 };
             }
+
+            _logger.LogInformation("Pricing background service has started");
+        }
+
+        // The Interlocked.Exchange makes our two operations (comparing and setting - incrementing) atomic 
+        private async Task StartGeneratorAsync() // Making start/stopping our generator thread-safe, and don't allow many threads to start/stop in parallel
+        {
+            if (Interlocked.Exchange(ref _runningStatus, 1) == 1) // We want to exchange (set) to 1 the variable, but if it's actually 1 then do nothing
+            {
+                _logger.LogInformation("Pricing generator is already running.");
+                return;
+            }
+            await _generator.StartAsync();
+        }
+
+        private async Task StopGeneratorAsync()
+        {
+            if (Interlocked.Exchange(ref _runningStatus, 0) == 0) // We want to exchange (set) to 0 the variable, but if it's actually 0 then do nothing
+            {
+                _logger.LogInformation("Pricing generator is not running.");
+                return;
+            }
+            await _generator.StopAsync();
         }
     }
 }
